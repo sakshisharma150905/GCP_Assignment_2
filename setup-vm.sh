6cf4ffb55ff18caab71b07ff86c074396ca6272f
b22cs073293 <sakshisharma150905@gmail.com>
2025-03-02 02:13:09 +0530

    Updated setup-vm.sh

--- setup-vm.sh
+++ setup-vm.sh
@@ -5,13 +5,13 @@ PROJECT_ID="eloquent-falcon-452317-r5"
 ZONE="us-central1-c"
 INSTANCE_NAME="sakshib22cs073"
 
-# Create a VM
+# Create a VM with Ubuntu 20.04
 gcloud compute instances create $INSTANCE_NAME \
     --project=$PROJECT_ID \
     --zone=$ZONE \
     --machine-type=e2-medium \
-    --image-family=debian-11 \
-    --image-project=debian-cloud \
+    --image-family=ubuntu-2004-lts \  
+    --image-project=ubuntu-os-cloud \ 
     --tags=http-server,https-server
 
 echo "VM Created: $INSTANCE_NAME in project $PROJECT_ID"
